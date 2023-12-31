0111b99b029e48f76dccf918c1b8c6027309dc2a
Ling <126978057+chenlingj999@users.noreply.github.com>
2023-12-31 16:47:30 -0500

    Set size of divs in css instead of js

--- EtchASketch.js
+++ EtchASketch.js
@@ -3,8 +3,6 @@ let gridSize = 16;
 let i = 0;
 while (i < gridSize ** 2) {
     const square = document.createElement("div");
-    square.style.width = '5px';
-    square.style.height = '5px';
     square.classList.add("grid");
     container.appendChild(square);
     i++;
