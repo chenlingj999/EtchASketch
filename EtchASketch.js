75b3d04a0926be59f14c1606754a4e8782eae1e7
Ling <126978057+chenlingj999@users.noreply.github.com>
2023-12-31 16:45:53 -0500

    Add class grid to each div created

--- EtchASketch.js
+++ EtchASketch.js
@@ -5,6 +5,7 @@ while (i < gridSize ** 2) {
     const square = document.createElement("div");
     square.style.width = '5px';
     square.style.height = '5px';
+    square.classList.add("grid");
     container.appendChild(square);
     i++;
 }
