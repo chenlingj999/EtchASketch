6db05721863bd6b5b13237d0810b72efa998a682
Ling <126978057+chenlingj999@users.noreply.github.com>
2023-12-31 21:14:45 -0500

    Create func to change size grid after button press

--- EtchASketch.js
+++ EtchASketch.js
@@ -1,18 +1,23 @@
 const container = document.querySelector('.container');
-let gridSize = 16;
-let i = 0;
-while (i < gridSize ** 2) {
-    const square = document.createElement("div");
-    square.classList.add("grid");
-    container.appendChild(square);
-    square.addEventListener(
-        "mouseover", 
-        (event) => {
-            event.target.style.backgroundColor = "black";
-        }
-    );
-    i++;
+function generateGrid(gridSize) {
+    let i = 0;
+    while (i < gridSize ** 2) {
+        const square = document.createElement("div");
+        const itemSize = (800 / gridSize)
+        square.style.width = `${itemSize}px`;
+        square.style.height = `${itemSize}px`;
+        square.classList.add("grid");
+        container.appendChild(square);
+        square.addEventListener(
+            "mouseover", 
+            (event) => {
+                event.target.style.backgroundColor = "black";
+            }
+        );
+        i++;
+    }
 }
+generateGrid(16);
 
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
@@ -50,3 +55,19 @@ function closeModal(modal) {
     modal.classList.remove('active');
     overlay.classList.remove('active');
 }
+
+const submitSize = document.querySelector('[data-submit-button]');
+submitSize.addEventListener('click', () => {
+    const input = document.getElementById('size');
+    const size = input.value;
+    input.value = '';
+
+    if (isNaN(size) || (size < 1 || size > 100)) return;
+    container.innerHTML = "";
+    generateGrid(size);
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+});
